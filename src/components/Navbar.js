1652123c0c626d72bdf70316eb7a6259fe53b48c
vmrecio <38523618+vmrecio@users.noreply.github.com>
2023-11-07 00:15:50 +0100

    Add files via upload

--- src/components/Navbar.js
+++ src/components/Navbar.js
@@ -42,7 +42,7 @@ const Navbar = class extends React.Component {
         <div className="container">
           <div className="navbar-brand">
             <Link to="/" className="navbar-item" title="Logo">
-              <img src={logo} alt="Kaldi" style={{ width: "88px" }} />
+              <img src={logo} alt="Victor" style={{ width: "88px" }} />
             </Link>
             {/* Hamburger menu */}
             <div
@@ -63,20 +63,11 @@ const Navbar = class extends React.Component {
             className={`navbar-menu ${this.state.navBarActiveClass}`}
           >
             <div className="navbar-start has-text-centered">
-              <Link className="navbar-item" to="/about">
-                About
-              </Link>
-              <Link className="navbar-item" to="/products">
-                Products
-              </Link>
               <Link className="navbar-item" to="/blog">
                 Blog
               </Link>
               <Link className="navbar-item" to="/contact">
-                Contact
-              </Link>
-              <Link className="navbar-item" to="/contact/examples">
-                Form Examples
+                Contacto
               </Link>
             </div>
             <div className="navbar-end has-text-centered">
