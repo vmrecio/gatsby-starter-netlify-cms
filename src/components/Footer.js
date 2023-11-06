ab608de63e622c3f025252f08400ff21cdc78e19
vmrecio <38523618+vmrecio@users.noreply.github.com>
2023-11-06 23:55:05 +0100

    Add files via upload

--- src/components/Footer.js
+++ src/components/Footer.js
@@ -2,10 +2,10 @@ import * as React from "react";
 import { Link } from "gatsby";
 
 import logo from "../img/logo.svg";
-import facebook from "../img/social/facebook.svg";
-import instagram from "../img/social/instagram.svg";
 import twitter from "../img/social/twitter.svg";
-import vimeo from "../img/social/vimeo.svg";
+import instagram from "../img/social/instagram.svg";
+import github from "../img/social/github.svg";
+import linkedin from "../img/social/linkedin.svg";
 
 const Footer = class extends React.Component {
   render() {
@@ -14,7 +14,7 @@ const Footer = class extends React.Component {
         <div className="content has-text-centered">
           <img
             src={logo}
-            alt="Kaldi"
+            alt="Víctor"
             style={{ width: "14em", height: "10em" }}
           />
         </div>
